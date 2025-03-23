adf76f39ca951d098665613c219f80691ef13a07
Daniel Tsagli <Danieltsagli960@gmail.com>
2025-03-23 14:51:21 +0000

    Update index.ts

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -6,7 +6,6 @@ import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-import apiRoutes from './api/index.js';
 
 import apiRoutes from './api/index.js';
 import htmlRoutes from './htmlRoutes.js';
