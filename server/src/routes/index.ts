dc211900296a850f16c7df140bd28f7011003429
Daniel Tsagli <Danieltsagli960@gmail.com>
2025-03-23 14:50:01 +0000

    Update index.ts

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,10 +1,20 @@
-import { Router } from 'express';
-const router = Router();
+import type { Request, Response } from 'express';
+import express from 'express';
+const router = express.Router();
+
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+import apiRoutes from './api/index.js';
 
 import apiRoutes from './api/index.js';
 import htmlRoutes from './htmlRoutes.js';
 
 router.use('/api', apiRoutes);
 router.use('/', htmlRoutes);
+router.use((_req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, '../../../client/dist/index.html'));
+});
 
 export default router;
